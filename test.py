8666add98fa7585c3e8b56d23c722cf0a0ad285a
Sansoeko <krijt.sander@gmail.com>
2017-06-29 02:35:55 +0200

    update

--- test.py
+++ test.py
@@ -1,1 +1,26 @@
-print "hallo"
+import gzip, cPickle
+import matplotlib.pyplot as plt
+
+def load_mnist():
+    f = gzip.open('mnist.pkl.gz', 'rb')
+    data = cPickle.load(f)
+    f.close()
+    return data
+
+
+def plot_digits(data, num_cols, shape=(28,28)):
+    num_digits = data.shape[0]
+    num_rows = int(num_digits / num_cols)
+    for i in range(num_digits):
+        plt.subplot(num_rows, num_cols, i + 1)
+        plt.axis('off')
+        plt.imshow(data[i].reshape(shape), interpolation='nearest', cmap='Greys')
+    plt.show()
+    
+
+if __name__=="__main__":
+	(x_train, t_train), (x_valid, t_valid), (x_test, t_test) = load_mnist()
+
+	#plot_digits(x_train[0:1], 1)
+
+	print t_train.shape
