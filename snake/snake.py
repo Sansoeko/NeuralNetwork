0d208ac01860e5950d3c87ffebe210e9a3b1539d
Sansoeko <krijt.sander@gmail.com>
2017-07-04 17:01:47 +0200

    insaneSnake

--- snake/snake.py
+++ snake/snake.py
@@ -6,15 +6,18 @@ import time
 import curses
 import numpy as np
 
-WIDTH = 30
-HEIGHT = 30
+WIDTH = 42
+HEIGHT = 42
 SNAKE_CHAR = "X"
-POWERUP_CHAR = "o"
+POWERUP_CHAR = "Q"
 EMPTY_CHAR = "."
-SPEED = 0.12  # Time between two frames
+WALL_CHAR = "#"
+SPEED = 0.24  # Time between two frames
 START_LENGTH = 6
 POWERUP = False #is powerup spawned?
 X = 0.2 #change powerup is spawned every frame
+LIVE = True
+SCORE = 0 #ik heb 26 gehaald d;
 
 
 def clean_grid():
@@ -23,6 +26,10 @@ def clean_grid():
     """
     grid = np.chararray((HEIGHT, WIDTH))
     grid[:] = EMPTY_CHAR
+    grid[0:HEIGHT,0] = WALL_CHAR
+    grid[0:HEIGHT,WIDTH-1] = WALL_CHAR
+    grid[0,1:WIDTH] = WALL_CHAR
+    grid[HEIGHT-1,1:WIDTH] = WALL_CHAR
     return grid
 
 
@@ -34,6 +41,7 @@ def print_grid(stdscr, grid):
         stdscr.addstr(" ".join(row) + "\n")
 
     stdscr.addstr("\nPress 'q' to quit.")
+    stdscr.addstr("\nYOUR SCORE: " + str(SCORE) + "!!!")
     stdscr.refresh()
     stdscr.move(0, 0)
 
@@ -44,7 +52,7 @@ def new_game():
     """
     grid = clean_grid()
     direction = key_to_direction(rd.randint(258, 261))
-    snake = [(rd.randint(1, HEIGHT - 1), rd.randint(1, WIDTH - 1))]  # row, col
+    snake = [(rd.randint(5, HEIGHT - 5), rd.randint(5, WIDTH - 5))]  # row, col
     for _ in range(START_LENGTH - 1):
         snake.append(next_pos(snake[-1], direction))
     return grid, snake, direction
@@ -79,19 +87,28 @@ def update_snake(grid, snake, direction):
     """
     Update the snake by removing from tail, adding the next position, and updating the grid.
     """
-    # TODO reset game when colliding with tail
     global POWERUP
+    global SPEED
+    global SCORE
+
     if grid[next_pos(snake[-1], direction)] == POWERUP_CHAR:
     	snake.append(next_pos(snake[-1], direction))
     	grid[snake[-1][0] % HEIGHT, snake[-1][1] % WIDTH] = SNAKE_CHAR
     	POWERUP = False
+    	if not SPEED < 0.04:
+    		SPEED = SPEED * 0.88
+    	SCORE = SCORE + 1
+    	return True
     elif grid[next_pos(snake[-1], direction)] == SNAKE_CHAR:
-    	grid, snake, direction = new_game()
+    	return False
+    elif grid[next_pos(snake[-1], direction)] == WALL_CHAR:
+    	return False
     else:
     	grid[snake[0][0] % HEIGHT, snake[0][1] % WIDTH] = EMPTY_CHAR
     	del snake[0]
     	snake.append(next_pos(snake[-1], direction))
     	grid[snake[-1][0] % HEIGHT, snake[-1][1] % WIDTH] = SNAKE_CHAR
+    	return True
 
 
 def spawn_powerup(grid):
@@ -99,12 +116,11 @@ def spawn_powerup(grid):
     """
     If there is not yet a powerup, with possibility of x, add powerup to grid.
     """
-    # TODO implement here
     global POWERUP
     global X
     if POWERUP == False:
     	if rd.random() < X:
-    		grid[(rd.randint(1, HEIGHT - 1), rd.randint(1, WIDTH - 1))] = POWERUP_CHAR
+    		grid[(rd.randint(2, HEIGHT - 2), rd.randint(2, WIDTH - 2))] = POWERUP_CHAR
     		POWERUP = True
 
 
@@ -115,7 +131,9 @@ def main(stdscr):
     """
     stdscr.nodelay(1)
     grid, snake, direction = new_game()
-    while True:
+    global LIVE
+    global SCORE
+    while LIVE:
         # Keyinput to change direction of snake.
         char = stdscr.getch()
         if char in range(258, 262):
@@ -125,12 +143,12 @@ def main(stdscr):
             break
 
         # Update game state
-        update_snake(grid, snake, direction)
+        LIVE = update_snake(grid, snake, direction)
         spawn_powerup(grid)
 
         print_grid(stdscr, grid)
         time.sleep(SPEED)
-
+    
 
 if __name__ == "__main__":
     curses.wrapper(main)
